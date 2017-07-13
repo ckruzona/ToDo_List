a2a78f6a2315d9135b299305d681b9e35c525a8e
ckruzona <chriskruzona@gmail.com>
2017-07-13 04:24:41 -0700

    Changed jQuery lib to correct version and added delete Todo logic

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,12 @@
-// Check Off Specific Todos By Clicking.
+// Check off specific Todos by clicking.
 $("li").click(function(){
 	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo.
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
